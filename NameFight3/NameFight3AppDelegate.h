1570abc099485af087e07db45ba803c2b10f35b9
nasawz <nasa127@gmail.com>
2011-03-18 12:01:52 +0800

    Initial Commit

---
+++ NameFight3/NameFight3AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  NameFight3AppDelegate.h
+//  NameFight3
+//
+//  Created by nasa on 11-3-18.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NameFight3AppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ NameFight3/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NameFight3
+//
+//  Created by nasa on 11-3-18.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
